4880ef9ab01d2058967a68a682c39059666ca03c
Wafae Chamour <wafae.chamour@gmail.com>
2023-06-30 19:15:54 -0700

    Function that adds two numbers.

--- 0x06-pointers_arrays_strings/103-infinite_add.c
+++ 0x06-pointers_arrays_strings/103-infinite_add.c
@@ -1,52 +1,51 @@
 #include "main.h"
 #include <stdio.h>
-
 /**
- * infinit_add - function that adds two numbers.
- * @n1: first number.
- * @n2: second number.
- * @r: the buffer that the function will use to store the result
- * @size_r: the buffer size.
- *
- * Return: pointer to r.
+ * infinite_add - adds two numbers
+ * @n1: number one.
+ * @n2: number two.
+ * @r: buffer that the function will use to store the result.
+ * @size_r: buffer size:
+ * Return: the pointer to dest.
  */
-char *infinit_add(char *n1, char *n2, char *r, int size_r)
-{
-	int i = 0, i2 = 0, j, j2, k, k2, l = 0;
 
-	while (*(n1 + i) != '\0')
-		i++;
-	while (*(n2 + i2) != '\0')
-		i2++;
-	if (i >= i2)
-		j2 = i;
+char *infinite_add(char *n1, char *n2, char *r, int size_r)
+{
+	int c1 = 0, c2 = 0, op, bg, dr1, dr2, add = 0;
+
+	while (*(n1 + c1) != '\0')
+		c1++;
+	while (*(n2 + c2) != '\0')
+		c2++;
+	if (c1 >= c2)
+		bg = c1;
 	else
-		j2 = i2;
-	if (size_r <= j2 + 1)
+		bg = c2;
+	if (size_r <= bg + 1)
 		return (0);
-	r[j2 + 1] = '\0';
-	i--, i2--, size_r--;
-	k = *(n1 + i) - 48, k2 = *(n2 + i2) - 48;
-	while (j2 >= 0)
+	r[bg + 1] = '\0';
+	c1--, c2--, size_r--;
+	dr1 = *(n1 + c1) - 48, dr2 = *(n2 + c2) - 48;
+	while (bg >= 0)
 	{
-		j = k + k2 + l;
-		if (j >= 10)
-			l = j / 10;
+		op = dr1 + dr2 + add;
+		if (op >= 10)
+			add = op / 10;
 		else
-			l = 0;
-		if (j > 0)
-			*(r + j2) = (j % 10) + 48;
+			add = 0;
+		if (op > 0)
+		*(r + bg) = (op % 10) + 48;
 		else
-			*(r + j2) = '0';
-		if (i > 0)
-			i--, k = *(n1 + i) - 48;
+			*(r + bg) = '0';
+		if (c1 > 0)
+			c1--, dr1 = *(n1 + c1) - 48;
 		else
-			k = 0;
-		if (i2 > 0)
-			i2--, k2 = *(n2 + i2) - 48;
+			dr1 = 0;
+		if (c2 > 0)
+			c2--, dr2 = *(n2 + c2) - 48;
 		else
-			k2 = 0;
-		j2--, size_r--;
+			dr2 = 0;
+		bg--, size_r--;
 	}
 	if (*(r) == '0')
 		return (r + 1);
