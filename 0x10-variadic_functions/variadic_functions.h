964f063a9a2a7233f6f01c8033a0f051e04a9a8f
Wafae Chamour <wafae.chamour@gmail.com>
2023-07-20 12:38:43 -0700

    README.md variadic_functions.h 3-print_all.c

--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -20,6 +20,7 @@ typedef struct args_t
 int sum_them_all(const unsigned int n, ...);
 void print_numbers(const char *separator, const unsigned int n, ...);
 void print_strings(const char *separator, const unsigned int n, ...);
+void print_all(const char * const format, ...);
 
 #endif /* VARIAFUNC */
 
