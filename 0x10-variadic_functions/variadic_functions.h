d796bd59bb17a57b9fec4b034abba70065257d34
Wafae Chamour <wafae.chamour@gmail.com>
2023-07-20 12:35:09 -0700

    README.md variadic_functions.h 2-print_strings.c

--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -19,6 +19,7 @@ typedef struct args_t
 
 int sum_them_all(const unsigned int n, ...);
 void print_numbers(const char *separator, const unsigned int n, ...);
+void print_strings(const char *separator, const unsigned int n, ...);
 
 #endif /* VARIAFUNC */
 
