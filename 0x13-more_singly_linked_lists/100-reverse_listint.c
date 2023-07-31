077b38b7c23a3acc3ab81aa037dd8beb5a659477
Chamour15 <wafae.chamour@gmail.com>
2023-07-31 15:00:39 -0700

    100-reverse_listint.c

--- 0x13-more_singly_linked_lists/100-reverse_listint.c
+++ 0x13-more_singly_linked_lists/100-reverse_listint.c
@@ -1,45 +1,29 @@
 /*
  * File: 10-delete_nodeint.c
- * Auth: Brennan D Baraban
  */
 
 #include "lists.h"
 
 /**
- * delete_nodeint_at_index - Deletes the node at a given
- *                           index of a listint_t list.
- * @head: A pointer to the address of the
- *        head of the listint_t list.
- * @index: The index of the node to be deleted - indices start at 0.
+ * reverse_listint - reverses a linked list
+ * @head: head of the list
  *
- * Return: On success - 1.
- *         On failure - -1.
+ * Return: the first node of the reversed node
  */
-int delete_nodeint_at_index(listint_t **head, unsigned int index)
+listint_t *reverse_listint(listint_t **head)
 {
-	listint_t *tmp, *copy = *head;
-	unsigned int node;
+	listint_t *next_node = NULL;
+	listint_t *prev_node = NULL;
 
-	if (copy == NULL)
-		return (-1);
-
-	if (index == 0)
+	if (head == NULL)
+		return (NULL);
+	while (*head != NULL)
 	{
-		*head = (*head)->next;
-		free(copy);
-		return (1);
+		next_node = (*head)->next;
+		(*head)->next = prev_node;
+		prev_node = *head;
+		*head = next_node;
 	}
-
-	for (node = 0; node < (index - 1); node++)
-	{
-		if (copy->next == NULL)
-			return (-1);
-
-		copy = copy->next;
-	}
-
-	tmp = copy->next;
-	copy->next = tmp->next;
-	free(tmp);
-	return (1);
+	*head = prev_node;
+	return (*head);
 }
