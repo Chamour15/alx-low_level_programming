7c40df8f7e48784ea3c72c63380dd9a29dcaa607
Chamour15 <wafae.chamour@gmail.com>
2023-08-07 16:42:14 -0700

    100-elf_header.c

--- 0x15-file_io/100-elf_header.c
+++ 0x15-file_io/100-elf_header.c
@@ -40,8 +40,18 @@ int main(int argc, char *argv[]) {
         }
     }
 
-    // ... (le reste du code reste inchangé) ...
-    
+    check_elf(header.e_ident);
+
+    printf("ELF Header:\n");
+    print_magic(header.e_ident);
+    print_class(header.e_ident);
+    print_data(header.e_ident);
+    print_version(header.e_ident);
+    print_osabi(header.e_ident);
+    print_abi(header.e_ident);
+    print_type(header.e_type, header.e_ident);
+    print_entry(header.e_entry, header.e_ident);
+
     close(fd);
     return 0;
 }
