4f097ab7c98805e21293518260b44c140e49c6d2
Chamour15 <wafae.chamour@gmail.com>
2023-08-07 16:31:43 -0700

    3-cp.c

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -6,6 +6,7 @@
 * main - program that copies the content of a file to another file
 * @argc: num argument
 * @argv: string argument
+* @description: program that copies the content of a file to another file
 * Return: 0
 */
 
