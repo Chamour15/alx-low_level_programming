b35065629753fa5c893c48c4c4e6ff3124c9258d
Chamour15 <wafae.chamour@gmail.com>
2023-08-07 16:51:38 -0700

    100-elf_header.c

--- 0x15-file_io/100-elf_header.c
+++ 0x15-file_io/100-elf_header.c
@@ -301,15 +301,17 @@ int main(int __attribute__((__unused__)) argc, char *argv[])
 		exit(98);
 	}
 
+
 	check_elf(header->e_ident);
-	print_magic(header.e_ident);
-	print_class(header.e_ident);
-	print_data(header.e_ident);
-	print_version(header.e_ident);
-	print_osabi(header.e_ident);
-	print_abi(header.e_ident);
-	print_type(header.e_type, header.e_ident);
-	print_entry(header.e_entry, header.e_ident);
+	printf("ELF Header:\n");
+	print_magic(header->e_ident);
+	print_class(header->e_ident);
+	print_data(header->e_ident);
+	print_version(header->e_ident);
+	print_osabi(header->e_ident);
+	print_abi(header->e_ident);
+	print_type(header->e_type, header->e_ident);
+	print_entry(header->e_entry, header->e_ident);
 
 
 	free(header);
