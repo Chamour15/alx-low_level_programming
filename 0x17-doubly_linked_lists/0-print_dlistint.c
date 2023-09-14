4fa8aff6866af9822e39ef4194b6f15ef0676e0a
Chamour15 <wafae.chamour@gmail.com>
2023-09-14 10:44:52 -0700

    0-print_dlistint.c

--- 0x17-doubly_linked_lists/0-print_dlistint.c
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -1,17 +1,21 @@
 #include "lists.h"
-
 size_t print_dlistint(const dlistint_t *h)
 {
-	const dlistint_t *node = h;
-	size_t cont = 0;
 
-	while (node)
+	int counter = 0;
+
+	if (h == NULL)
+		return (counter);
+
+	while (h->prev != NULL)
+		h = h->prev;
+
+	while (h != NULL)
 	{
-		printf("%i\n", node->n);
-		cont++;
-		node = node->next;
+		printf("%d\n", h->n);
+		h = h->next;
+		counter++;
 	}
 
-	return (cont);
+	return (counter);
 }
-
